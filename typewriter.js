181be4cab80c909261f0dfcac10075a37322050c
bbashcode <binbash.code@gmail.com>
2022-01-18 21:21:51 +0000

    completed the typeWriter function for Animated Sentence task of W02D2

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,18 @@
 const sentence = "hello there from the lighthouse lab";
 
- let count = 0;
- for (const char of sentence) {
-   if(sentence.length){
-    process.stdout.write('\n');
-   }
-   setTimeout(() => {
-   process.stdout.write(char);  
-   }, count);
-  count += 50;
-}
+const typeWriter = function (str){
+  let count = 0;
+  for (const char of str) {
+    setTimeout(() => {
+    process.stdout.write(char);  
+    }, count);
+   count += 50;
+  }
+
+  let finalTimeOut = str.length * 50;
+  setTimeout(() => {
+    process.stdout.write('\n');  
+    }, finalTimeOut);
+};
+
+console.log(typeWriter(sentence));
